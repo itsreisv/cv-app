2ebd187eaddf9a916513ebfe5a106fc2b5b60518
itsreisv <123587174+itsreisv@users.noreply.github.com>
2023-10-19 13:14:56 -0400

    test

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -9,9 +9,9 @@ export function Preview({text, email, phone, location, list, data}) {
   <div className='header'>
     <div className='name'><h1>{text}</h1></div>
     <div className="info">
-      <div className='email'><img src="../src/assets/mail.svg" className="header-icon"/>{email}</div>
-      <div className='phone'><img src="../src/assets/phone.svg" className="header-icon" />{phone}</div>
-      <div className='location'><img src="../src/assets/location.svg" className="header-icon" />{location}</div>
+      <div className='email'><img src="./src/assets/mail.svg" className="header-icon"/>{email}</div>
+      <div className='phone'><img src="./src/assets/phone.svg" className="header-icon" />{phone}</div>
+      <div className='location'><img src="./src/assets/location.svg" className="header-icon" />{location}</div>
     </div>
   </div>
   <div className="education">
